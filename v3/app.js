1b7c1d123e53ab95c3ca038a8846a9c726ae9fce
FIRAT TALE <34514974+firattale@users.noreply.github.com>
2018-06-18 10:03:52 +0300

    Add files via upload

---
+++ v3/app.js
@@ -0,0 +1,77 @@
+var express        = require("express");
+var app            = express();
+var bodyParser     = require("body-parser");
+var mongoose       = require("mongoose");
+var Campground     = require("./models/campground");
+var seedDB         = require("./seeds");
+
+mongoose.connect("mongodb://localhost/yelp_camp_v3");
+app.use (bodyParser.urlencoded({extended:true }));
+app.set("view engine","ejs");
+seedDB();
+
+app.get("/", function (req,res) {
+    res.render("landing");
+});
+
+
+// SCHEMA SETUP
+
+//   INDEX - SHOW ALL CAMPGROUNDS
+app.get("/campgrounds", function (req,res) {
+    // Get all campgrounds from DB
+    Campground.find({},function (err,allCampgrounds) {
+        if (err) {
+            console.log(err);
+                 }   else {
+                res.render("index", {campgrounds:allCampgrounds});
+                          }
+    });
+});
+
+// CREATE - ADD NEW CAMPGROUND TO DB
+app.post("/campgrounds", function (req,res) {
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name:name, image:image, description:desc};
+    // Create a new campground and save to DB
+    Campground.create(newCampground,function (err,newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else{
+                // redirect back to campgrounds page
+                res.redirect("/campgrounds");
+              }
+    });
+});
+
+// NEW - SHOW FORM TO CREATE NEW CAMPGROUND
+app.get("/campgrounds/new", function (req,res) {
+    res.render("new.ejs");
+});
+
+// SHOW -SHOWS MORE INFO ABOUT A CAMPGROUND
+app.get("/campgrounds/:id",function (req,res) {
+    // find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
+        if (err) 
+        {
+            console.log(err);
+        }else{
+            console.log(foundCampground);
+             // render show template with that campground
+             res.render("show", {campground:foundCampground});
+             }
+    });
+});
+
+
+
+// Tell express to listen for requests (start server)
+
+app.listen(process.env.PORT, process.env.IP, function () {
+console.log("YelpCamp has started!!!"
+)}
+);
